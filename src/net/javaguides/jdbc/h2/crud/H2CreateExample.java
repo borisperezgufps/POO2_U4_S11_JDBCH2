5306c38215a14ee5ea856f5503ec225bbfcf4c7b
borisperezg <borisperezg@gmail.com>
2022-05-08 12:08:56 -0500

    Inclusion de update y delete

--- src/net/javaguides/jdbc/h2/crud/H2CreateExample.java
+++ src/net/javaguides/jdbc/h2/crud/H2CreateExample.java
@@ -1,15 +1,28 @@
 package net.javaguides.jdbc.h2.crud;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.swing.JOptionPane;
+
 public class H2CreateExample {
-	
 
 	public static void main(String[] argv) throws SQLException {
-		H2CreateExample createTableExample = new H2CreateExample();
-		createTableExample.createTable();
+		H2CreateExample h2Example = new H2CreateExample();
+		h2Example.createTable();
+		
+		String valId = JOptionPane.showInputDialog("Ingrese el ID: ");
+		String valName = JOptionPane.showInputDialog("Ingrese el Nombre: ");
+		String valEmail = JOptionPane.showInputDialog("Ingrese el Email: ");
+		String valCountry = JOptionPane.showInputDialog("Ingrese el Pais: ");
+		String valContra = JOptionPane.showInputDialog("Ingrese el Contrasena: ");
+		
+		h2Example.insertRecord(valId, valName, valEmail, valCountry, valContra);
+		h2Example.showRecords();
+		
+		
 	}
 
 	public void createTable() throws SQLException {
@@ -18,7 +31,7 @@ public class H2CreateExample {
 		sbCreateTableSQL.append("create table users (id int primary key, ");
 		sbCreateTableSQL.append("name varchar(20), email varchar(20), ");
 		sbCreateTableSQL.append("country varchar(20), password varchar(20))");
-		
+
 		System.out.println(sbCreateTableSQL);
 		// Step 1: Establishing a Connection
 		try (Connection connection = H2JDBCUtils.getConnection();
@@ -30,27 +43,136 @@ public class H2CreateExample {
 
 		} catch (SQLException e) {
 			// print SQL exception information
-			e.printStackTrace();
+			System.err.println("Ya está creada la tabla");
 		} finally {
 //			insertRecord();
 		}
 	}
 
-	public void insertRecord() throws SQLException {
-		
+	public void insertRecord(String id, String name, String email, String country, String contra) throws SQLException {
 
 		Connection conn = null;
 		Statement statementOb = null;
-		
+
 		try {
 			conn = H2JDBCUtils.getConnection();
 			statementOb = conn.createStatement();
 
 			StringBuffer sb = new StringBuffer();
 			sb.append("INSERT INTO USERS(id, name, email, country, password) ");
-			sb.append("VALUES (2, 'boris', 'correo', 'Colombia', '12345')");
+			sb.append("VALUES ("+id+", '"+name+"', '"+email+"', '"+country+"', '"+contra+"')");
 
 			statementOb.executeUpdate(sb.toString());
+
+		} catch (SQLException e) {
+			// print SQL exception information
+			e.printStackTrace();
+		} finally {
+			// Close the connection
+			try {
+				statementOb.close();
+				conn.close();
+			} catch (SQLException e) {
+				// print SQL exception information
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+	
+	public void updateRecord(String id, String name, String email, String country, String contra) throws SQLException {
+
+		Connection conn = null;
+		Statement statementOb = null;
+
+		try {
+			conn = H2JDBCUtils.getConnection();
+			statementOb = conn.createStatement();
+
+			StringBuffer sb = new StringBuffer();
+			sb.append("UPDATE USERS SET name = '"+name+"', email = '"+email+"', country='"+country+"', password='"+contra+"' ");
+			sb.append("WHERE id="+id+"");
+
+			statementOb.executeUpdate(sb.toString());
+
+		} catch (SQLException e) {
+			// print SQL exception information
+			e.printStackTrace();
+		} finally {
+			// Close the connection
+			try {
+				statementOb.close();
+				conn.close();
+			} catch (SQLException e) {
+				// print SQL exception information
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+	
+	public void deleteRecord(String id) throws SQLException {
+
+		Connection conn = null;
+		Statement statementOb = null;
+
+		try {
+			conn = H2JDBCUtils.getConnection();
+			statementOb = conn.createStatement();
+
+			StringBuffer sb = new StringBuffer();
+			sb.append("DELETE FROM USERS WHERE id="+id+"");
+
+			statementOb.executeUpdate(sb.toString());
+
+		} catch (SQLException e) {
+			// print SQL exception information
+			e.printStackTrace();
+		} finally {
+			// Close the connection
+			try {
+				statementOb.close();
+				conn.close();
+			} catch (SQLException e) {
+				// print SQL exception information
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public void showRecords() throws SQLException {
+
+		Connection conn = null;
+		Statement statementOb = null;
+
+		try {
+			conn = H2JDBCUtils.getConnection();
+			statementOb = conn.createStatement();
+
+			String sql = "SELECT id, name, email, country FROM users";
+			ResultSet rs = statementOb.executeQuery(sql);
+			
+			while(rs.next()) { 
+	            // Retrieve by column name 
+	            int id  = rs.getInt("id"); 
+	            String name = rs.getString("name"); 
+	            String email = rs.getString("email"); 
+	            String country = rs.getString("country");  
+	            
+	            // Display values
+	            System.out.println("User "+ id); 
+	            System.out.println("----------------------");
+	            System.out.println("Name: " + name); 
+	            System.out.println("Email: " + email); 
+	            System.out.println("Country: " + country);
+	            System.out.println();
+	         } 
+			
+			rs.close();
 
 		} catch (SQLException e) {
 			// print SQL exception information
